db266348a10d695c6084a33fbaf93390159f0e39
GaoYuan <admin@MacBook-Air.local>
2019-04-20 21:47:02 +0800

    在牛客评论区看到的一道题
    很有意思，但是还没写。。

---
+++ InterviewQuestions/src/April20th/niukepinglun/ChineseToDigits.java
@@ -0,0 +1,28 @@
+package April20th.niukepinglun;
+/**
+ * 牛客网评论区看到的一个题目
+ * @author GaoYuan
+ *例如：五千三百万零五百零一。
+我们一般需要把它转化成int型整数53000501
+需要一个单位栈，和一个数字栈，
+在用map存放亿，万，千
+ */
+public class ChineseToDigits {
+	/**
+	 * 
+	 * @param chinese
+	 * @return
+	 */
+	public long chineseToDigits(String chinese) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 530353000501
+		// 假设最多十二位
+		String s = "五千三百零三亿五千三百万零五百零一";
+		System.out.println(s.length());
+	}
+
+}
